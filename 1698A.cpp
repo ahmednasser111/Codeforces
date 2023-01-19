8e670d8d4514751610d3e7e65dbaaa30eac4acd9
ahmednaser111 <ahmednaser7707@gmail.com>
2023-01-19 10:28:46 +0200

    1698A solution added

---
+++ 1698A.cpp
@@ -0,0 +1,16 @@
+// XOR Mixup
+#include <bits\stdc++.h>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+  int t;
+  scanf("%d", &t);
+  while (t--)
+  {
+    int n, x;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+      scanf("%d", &x);
+    cout << x << endl;
+  }
+}
