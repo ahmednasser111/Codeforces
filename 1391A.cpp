11a490fdf9e68163e1c3c4fa616c28cd9a5db06a
ahmednaser111 <ahmednaser7707@gmail.com>
2023-02-02 21:25:20 +0200

    1391A solution added

---
+++ 1391A.cpp
@@ -0,0 +1,16 @@
+// Suborrays
+#include <bits\stdc++.h>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+  int t;
+  scanf("%d", &t);
+  while (t--)
+  {
+    int n;
+    scanf("%d", &n);
+    for (int i = n; i > 0; i--)
+      cout << i << " ";
+    cout << endl;
+  }
+}
