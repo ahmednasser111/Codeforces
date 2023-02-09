826ac4fb5b0ed64edda7cea2681eb77011ed13df
ahmednaser111 <ahmednaser7707@gmail.com>
2023-02-09 17:41:20 +0200

    1768A solution added

---
+++ 1768A.cpp
@@ -0,0 +1,14 @@
+// Greatest Convex
+#include <bits\stdc++.h>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+  int t;
+  scanf("%d", &t);
+  while (t--)
+  {
+    int k;
+    scanf("%d", &k);
+    cout << k - 1 << '\n';
+  }
+}
