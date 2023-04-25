d3882b6de38c62168e7a76e707ee61ae5c6972fa
ahmednaser111 <ahmednaser7707@gmail.com>
2023-04-25 02:40:26 +0200

    579A solution added

---
+++ 579A.cpp
@@ -0,0 +1,9 @@
+// Raising Bacteria
+#include <bits\stdc++.h>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+      int x;
+      scanf("%d", &x);
+      cout << __builtin_popcount(x);
+}
