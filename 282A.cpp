6d132afefcebaa296e5e7651e598106816e18082
ahmednaser111 <ahmednaser7707@gmail.com>
2023-06-15 15:09:08 +0300

    282A solution added

--- 282A.cpp
+++ 282A.cpp
@@ -1,19 +1,18 @@
 // Bit++
 #include <bits\stdc++.h>
 using namespace std;
-int main(int argc, char const *argv[])
+int main()
 {
   int n, x = 0;
-  string statement;
   scanf("%d", &n);
-  for (int i = 0; i < n; i++)
+  while (n--)
   {
-    cin >> statement;
-    if (statement == "++X" || statement == "X++")
+    string s;
+    cin >> s;
+    if (s[0] == '+' || s[2] == '+')
       x++;
     else
       x--;
   }
   cout << x;
-  return 0;
 }
