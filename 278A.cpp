4c4002878ab596f863d688ef434f547a4f0fe29c
ahmednaser111 <ahmednaser7707@gmail.com>
2023-09-27 07:15:31 +0300

    278A solution added

--- 278A.cpp
+++ 278A.cpp
@@ -3,22 +3,19 @@
 using namespace std;
 int main()
 {
-	int n, r, l, rs = 0, ls = 0;
+	int n, r, l, rs = 0, sum = 0;
 	scanf("%d", &n);
 	int a[n];
 	for (int i = 0; i < n; i++)
+	{
 		scanf("%d", &a[i]);
+		sum += a[i];
+	}
 	scanf("%d%d", &r, &l);
 	if (r > l)
 		swap(r, l);
-	r--;
-	l--;
-	for (int i = r; i < l; i++)
+	for (int i = r - 1; i < l - 1; i++)
 		rs += a[i];
-	for (int i = l; i != r; i++)
-		if (i == n)
-			i = -1;
-		else
-			ls += a[i];
-	cout << min(rs, ls);
+
+	cout << min(rs, sum - rs);
 }
