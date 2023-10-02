592134c3b5d9b4dfe799776c3e54bb8cb0b35446
ahmednaser111 <ahmednaser7707@gmail.com>
2023-10-02 12:23:03 +0300

    221A solution added

---
+++ 221A.cpp
@@ -0,0 +1,11 @@
+// Little Elephant and Function
+#include <bits\stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	cout << n << ' ';
+	for (int i = 1; i <= n - 1; i++)
+		cout << i << ' ';
+}
